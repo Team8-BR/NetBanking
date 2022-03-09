a343de97772f1394d79c31e4245fd7644dae138a
sunilskcj <sunilskcj@gmail.com>
2022-03-09 23:41:54 +0530

    Login page directive created

--- Client/src/app/net-banking/net-banking-routing.module.ts
+++ Client/src/app/net-banking/net-banking-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { CreateAccountComponent } from "./create-account/create-account.component";
+import { HomePageComponent } from "./home-page/home-page.component";
 import { LoginComponent } from "./login/login.component";
 import { RegistrationComponent } from "./registration/registration.component";
 
 const AppRoutes : Routes = [
+    {path : '', component : HomePageComponent},
     {path : 'Login', component : LoginComponent},
     {path : 'Register', component : RegistrationComponent},
     {path : 'CreateAccount', component : CreateAccountComponent}    
