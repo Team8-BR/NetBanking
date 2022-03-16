84ef7d1f73c47980688e808898210048db9a710f
harshapatil22 <98938197+harshapatil22@users.noreply.github.com>
2022-03-17 04:42:22 +0530

    form validation done

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CreateAccountComponent } from './net-banking/create-account/create-account.component';
+
+
 import { NetBankingModule } from './net-banking/net-banking.module';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateAccountComponent
+    
+    
     
   
   ],
   imports: [
-    BrowserModule,NetBankingModule,RouterModule
+    BrowserModule,
+    NetBankingModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Client/src/app/net-banking/create-account/create-account.component.ts
+++ Client/src/app/net-banking/create-account/create-account.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
+import {  FormBuilder,FormGroup,Validators } from '@angular/forms';
 
-@Component({
+@Component(
+  {
   selector: 'app-create-account',
   templateUrl: './create-account.component.html',
   styleUrls: ['./create-account.component.css']
-})
+  })
+  
 export class CreateAccountComponent implements OnInit {
 
-  constructor() { }
+  login =FormGroup=this._fb.group({    
+      Title:['', [Validators.required] ],
 
-  ngOnInit(): void {
+      FirstName:['',[Validators.required]],
+
+      MiddleName:['',[Validators.required]],
+
+      LastName:['',[Validators.required]],
+
+      FathersName:['',[Validators.required]],
+
+      MobileNumber:['',[Validators.required,
+      Validators.maxLength(10)]],
+
+      EmailID:['',[Validators.required,
+      Validators,]],
+
+      AadharCardNumber:['',[Validators.required,
+      Validators.maxLength(12)]],
+
+      DateofBirth:['',[Validators.required]],
+
+      AddressLine1:['',[Validators.required]],
+
+      AddressLine2:['',[Validators.required]],
+
+      Landmark:['',[Validators.required]],
+
+      State:['',[Validators.required]],
+
+      City:['',[Validators.required]],
+
+      Pincode:['',[Validators.required]],
+
+      
+
+    });
+  
+  constructor(private _fb:FormBuilder){}
+  ngOnInit():void{
+
   }
-
+  submitData(){
+    console.log(this.login)
+  }
+  
+    
 }
