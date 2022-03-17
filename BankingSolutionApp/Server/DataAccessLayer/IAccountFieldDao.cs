a189862d184d57f76426f46212102f50a25466b5
sunilskcj <sunilskcj@gmail.com>
2022-03-17 11:27:54 +0530

    Dependency injection Done

---
+++ BankingSolutionApp/Server/DataAccessLayer/IAccountFieldDao.cs
@@ -0,0 +1,14 @@
+﻿using Server.BusinessModels;
+using Server.Repository;
+using System.Collections.Generic;
+
+namespace Server.DataAccessLayer
+{
+    public interface IAccountFieldDao
+    {
+        bool AddAccountDetail(AccountFields account);
+        AccountField FetchAccountById(int id);
+        List<AccountField> FetchAllAccount();
+        bool InsertAccountField(AccountField p);
+    }
+}
