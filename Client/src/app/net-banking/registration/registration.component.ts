beb11b18237aecea85f571002dbf6eb0e66f892d
Manikandan2099 <98934300+Manikandan2099@users.noreply.github.com>
2022-03-20 13:00:48 +0530

    validation updated

--- Client/src/app/net-banking/registration/registration.component.ts
+++ Client/src/app/net-banking/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ConfirmedValidator } from '../customvalidators/confirm-password';
 import { passwordValidator } from '../customvalidators/password-validators';
 
 @Component({
@@ -14,11 +15,17 @@ export class RegistrationComponent implements OnInit {
   registration:FormGroup= this.fb.group({
     AccountNumber : ['',[Validators.required]],
     SetLoginPassword:['',[Validators.required,
-                          passwordValidator(6,10)]]
+                          passwordValidator(6,10)]],
+    ConfirmLoginPassword:['',[Validators.required,passwordValidator(6,10)]],
+    SetTransactionPassword:['',[Validators.required,
+                                passwordValidator(6,10)]],
+    ConfirmTransactionPassword:['',[Validators.required,ConfirmedValidator,passwordValidator(6,10)]]                            
   })
 
 
   ngOnInit(): void {
   }
-
+  submitData() {
+    console.log(this.registration)
+  }
 }
